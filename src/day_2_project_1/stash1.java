0df5b3de1351b71171a3bd0557b6b5b609102137
zaki <zaki@localhost>
2023-04-09 23:21:34 -0700

    project_1 oF hw

---
+++ src/day_2_project_1/stash1.java
@@ -0,0 +1,14 @@
+package day_2_project_1;
+
+public class stash1 {
+	public static void main(String[] args) {
+		System.out.println("line one before stash");
+	}
+
+	public boolean isValid() {
+	
+		boolean result = false;
+	
+		return result;	
+}
+	}
